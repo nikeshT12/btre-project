92526334eec8da83964cb4c785ded9c977959691
nikeshT12 <nikeshtandel12@gmail.com>
2019-01-24 20:24:48 +0000

    Removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -124,11 +124,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'nikeshtandel12@gmail.com'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
